2850cfaeffa60fc70dcaaa9fda75c8ee1aa8b30d
ZdeZhao <5134793961@qq.com>
2017-08-21 11:40:19 +0800

    configuration file

---
+++ node_modules/@ionic-native/core/ionic-native-plugin.js
@@ -0,0 +1,27 @@
+export var IonicNativePlugin = (function () {
+    function IonicNativePlugin() {
+    }
+    /**
+     * Returns a boolean that indicates whether the plugin is installed
+     * @return {boolean}
+     */
+    IonicNativePlugin.installed = function () { return false; };
+    /**
+     * Returns the original plugin object
+     */
+    IonicNativePlugin.getPlugin = function () { };
+    /**
+     * Returns the plugin's name
+     */
+    IonicNativePlugin.getPluginName = function () { return; };
+    /**
+     * Returns the plugin's reference
+     */
+    IonicNativePlugin.getPluginRef = function () { return; };
+    /**
+     * Returns the plugin's install name
+     */
+    IonicNativePlugin.getPluginInstallName = function () { return; };
+    return IonicNativePlugin;
+}());
+//# sourceMappingURL=ionic-native-plugin.js.map
